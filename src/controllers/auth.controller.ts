f8e22f13c9e0dccce3c6947729a63e3e16d8429e
Zikri Nur Iman <iman3329@gmail.com>
2025-05-25 23:38:41 +0700

    add fitur registration
    add fitur registration

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,57 @@
+import { Request, Response } from "express";
+
+import * as Yup from "yup";
+
+import UserModel from "../models/user.models";
+type TRegister = {
+  fullName: string;
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
+const registerValidateSchema = Yup.object({
+  fullName: Yup.string().required(),
+  username: Yup.string().required(),
+  email: Yup.string().required(),
+  password: Yup.string().required(),
+  confirmPassword: Yup.string()
+    .required()
+    .oneOf([Yup.ref("password"), ""], "Password must be matched"),
+});
+
+export default {
+  async register(req: Request, resp: Response) {
+    const { fullName, username, email, password, confirmPassword } =
+      req.body as unknown as TRegister;
+
+    try {
+      await registerValidateSchema.validate({
+        fullName,
+        username,
+        email,
+        password,
+        confirmPassword,
+      });
+
+      const result = await UserModel.create({
+        fullName,
+        email,
+        username,
+        password,
+      });
+
+      resp.status(200).json({
+        message: "Success registration",
+        data: result,
+      });
+    } catch (error) {
+      const err = error as unknown as Error;
+      resp.status(400).json({
+        message: err.message,
+        data: null,
+      });
+    }
+  },
+};
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
 import express from "express";
+import bodyParser from "body-parser";
 import router from "./routes/api";
+import db from "./utils/database";
 
-const app = express();
+async function init() {
+  try {
+    const result = await db();
+    console.log("database status:", result);
+    const app = express();
 
-const PORT = 3000;
+    app.use(bodyParser.json());
 
-app.use("/api", router);
+    const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log(`server is running on http://localhost:${PORT}`);
-});
+    app.use("/api", router);
+
+    app.listen(PORT, () => {
+      console.log(`server is running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+init();
